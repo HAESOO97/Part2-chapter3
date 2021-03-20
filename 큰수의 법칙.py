a2abcd50418bb50474f99545e9c6fee69e6d5cbe
HAESOO97 <79429010+HAESOO97@users.noreply.github.com>
2021-03-20 14:13:49 +0900

    Update 큰수의 법칙.py

--- 큰수의 법칙.py
+++ 큰수의 법칙.py
@@ -1,21 +1,38 @@
-n,m,k = map(int, input().split())
-data = list(map(int ,input().split()))
+#방법1
 
-data.sort()
+n,m,k = map(int,input().split())
 
-max_value = data[n-1];
-second_value = data[n-2];
+data = list(map(int,input().split()))
+first = data[n-1]
+second = data[n-2]
 
-result = 0;
+result = 0
 
 while True:
-  for i in range(k):
-    if m==0:
+  for i in range(K):
+    if m == 0:
       break
-    result+=max_value
+    result+=first
     m-=1
-  if m==0:
+   if m == 0:
     break
-  result+=second_value
-  m-=1
+   result+=second
+   m-=1
 print(result)
+
+#방법
+n,m,k = map(int,input().split())
+data = list(map(int,input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+#가장큰수가 더해지는 횟수 계산
+count = int(m/(k+1))*k
+count += m%(k+1)
+result = 0
+result += (count)*first
+result +=(m-count)*second
+print(result)
+
